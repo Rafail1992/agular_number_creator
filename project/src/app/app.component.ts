acd47e74c1eab7b7d35b02abde8b403d8ed419dc
Rafail Dardagiannopoulos <77059808+Rafail1992@users.noreply.github.com>
2021-02-02 18:39:29 +0200

    Version 1.1

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'Angular Number Creator';
+  creator = "Rafail1992";
 }
